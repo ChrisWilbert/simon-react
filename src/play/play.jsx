dacedc3dce7bb062ae02f135c62a66ecc062f01b
ChrisWilbert <chriswilbert54@gmail.com>
2024-12-11 21:45:00 -0700

    new react commit

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -7,7 +7,6 @@ export function Play(props) {
   return (
     <main className='bg-secondary'>
       <Players userName={props.userName} />
-      <SimonGame userName={props.userName} />
       <h1>I want to cry </h1>
     </main>
   );
